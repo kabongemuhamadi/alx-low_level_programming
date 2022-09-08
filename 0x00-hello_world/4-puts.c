42debabffa14a0b20c634d565c195e7a84e669f5
kabongemuhamadi <kabongemuhamadi@gmail.com>
2022-09-08 09:20:52 -0700

    compilled

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
-/*This program prints 
-*the output using puts
-
-*/
-int main(void){
-  puts("\"programming is like building multilingual puzzle\n");
-  return 0;
-
+// this prints the output
+int main(void)
+{ puts("\"programming is like building multilingual puzzle\n");
+ return (0);
 }
